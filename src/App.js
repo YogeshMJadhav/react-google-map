e8b36c7cc59163d4e09f1de8d7970273863b1a55
Yogesh Jadhav <yogesh18.ivision@gmail.com>
2019-08-07 13:43:02 +0530

    update key

--- src/App.js
+++ src/App.js
@@ -60,5 +60,5 @@ if (this.state.showingInfoWindow) {
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCT3DSaZmheznyoQ1KGNo01HSx7KjGZn8U'
+  apiKey: 'Paste_here_your_Google_API_key'
 })(App);
